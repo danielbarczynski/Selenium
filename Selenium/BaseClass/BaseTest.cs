f51befba1502fdebe07fff9fbf3977d25b88fa05
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-07-15 15:40:08 +0200

    movie1 done

---
+++ Selenium/BaseClass/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.BaseClass
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.margonem.pl/";
+        }
+        
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit(); 
+        }
+    }
+}
--- Selenium/UnitTest1.cs
+++ Selenium/UnitTest1.cs
@@ -1,9 +1,10 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using Selenium.BaseClass;
 namespace Selenium
 {
-    public class Tests
+    [TestFixture]
+    public class Tests : BaseTest
     {
         [SetUp]
         public void Setup()
@@ -13,8 +14,23 @@ namespace Selenium
         [Test]
         public void Test1()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.margonem.pl/";
+            IWebElement input = driver.FindElement(By.Name("login"));
+            IWebElement password = driver.FindElement(By.Name("password"));
+            IWebElement password2 = driver.FindElement(By.Id("popup-create-account-password2"));
+            IWebElement checkbox = driver.FindElement(By.Id("popup-create-account-checkbox"));
+            IWebElement checkbox2 = driver.FindElement(By.Id("popup-create-account-checkbox-openid-rodo"));
+            IWebElement button = driver.FindElement(By.ClassName("button-register"));
+            input.SendKeys("dalksdnasjn");
+            password.SendKeys("dalksdnasjn23");
+            password2.SendKeys("dalksdnasjn23");
+            checkbox.Click();
+            checkbox2.Click();
+            button.Click();
+            Thread.Sleep(5000);
+        }
+        [Test]
+        public void Test2()
+        {
             IWebElement input = driver.FindElement(By.Name("login"));
             IWebElement password = driver.FindElement(By.Name("password"));
             IWebElement password2 = driver.FindElement(By.Id("popup-create-account-password2"));
@@ -28,5 +44,6 @@ namespace Selenium
             checkbox2.Click();
             button.Click();
         }
+
     }
 }
